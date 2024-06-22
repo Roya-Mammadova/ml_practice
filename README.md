354329c6d7ebc1f9d48fdecd81a138720e101074
Roya-Mammadova <mroyava@gmail.com>
2024-06-22 18:47:15 +0400

    file updated

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ source avtivate ./env
 
 '''
 git init
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_Dot='-e.'
+def get_requiremet(file_path:str)->List[str]:
+    requirements= []
+    with open(file_path) as f:
+        requirements=f.readline()
+        requirements= [req.replace("\n","")for req in requirements]
+        
+        
+        if HYPEN_E_Dot in requirements:
+            requirements.remove(HYPEN_E_Dot)
+    return requirements
+
+
+setup(
+    name="src",
+    version="0.01",
+    author="Roya",
+    author_email="mroyava@gmail.com",
+    install_requires=get_requiremet("requirements.txt"),
+    packages=find_packages()
+)
